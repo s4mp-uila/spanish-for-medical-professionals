1d7cf71dab12082fe498c03a3887f264cd55352c
Csongor Czezar <csongor.czezar@gmail.com>
2020-08-30 13:11:17 -0700

    Created Navbar links

--- client/src/Components/Navigation/Navigation.js
+++ client/src/Components/Navigation/Navigation.js
@@ -1,9 +1,16 @@
 import React from "react"
+import {Link} from "react-router-dom"
 import "./Navigation.css"
 
 function Navigation() {
     return (
-        <h1>Navigation</h1>
+        <nav>
+            <Link to="/">Home</Link>
+            <Link to="/courses">Courses</Link>
+            <Link to="/about">About Us</Link>
+            <Link to="/teacher">Hire a Teacher</Link>
+            <Link to="/login">Login / Sign up</Link>
+        </nav>
     )
 }
 
