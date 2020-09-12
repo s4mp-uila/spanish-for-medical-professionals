368f7b25fd2fa191ae58a1510b8168b1efa06e38
jangelroa <jangelroa@gmail.com>
2020-09-11 20:24:34 -0700

    Added sass recognition

--- client/src/Components/Navigation/Navigation.js
+++ client/src/Components/Navigation/Navigation.js
@@ -1,7 +1,7 @@
 import React,{useContext} from "react"
 import {Link} from "react-router-dom"
 import {StateContext} from "../Context/GlobalState"
-import "./Navigation.css"
+import "./Navigation.scss"
 
 function Navigation() {
     const {tempName,login,Logout} = useContext(StateContext)
