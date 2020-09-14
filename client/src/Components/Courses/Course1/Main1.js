f8de36b2e65ef68e2a3b21444b1054d448dafff4
Csongor Czezar <csongor.czezar@gmail.com>
2020-09-13 19:10:52 -0700

    Used props to make the Children components even more reusable

--- client/src/Components/Courses/Course1/Main1.js
+++ client/src/Components/Courses/Course1/Main1.js
@@ -1,27 +1,21 @@
 import React from "react"
 import Main from "../CourseChildren/Main"
-import {useHistory} from "react-router-dom"
 import "../Courses.css"
 
-function Main1() {
+function Main1(props) {
 
-    const history = useHistory();
-    function toVideo(e) {
-      e.preventDefault()
-      history.push("/course1/video1")
-    }
+    
 
   return (
     <div>
-      <Main>
+      <Main url = "/course1/video1">
         <h1>SpanishForProfessionals</h1>
         <h3>Learning Spanish with <b>SpanishForProfessionals</b> is entirely free. 
             We are available for classroom or home with dozens of games, 
             coloring pages and interactive activities ordered by levels that you can access anytime 
             in tablets or desktops.</h3>
             <p>After completion of each part, just click on the NEXT button to go forward</p>
-        <button className="cancel">Cancel</button>
-        <button onClick={toVideo} className="next">Next</button>
+        
        </Main>
     </div>
   )
--- client/src/Components/Courses/CourseChildren/Main.js
+++ client/src/Components/Courses/CourseChildren/Main.js
@@ -1,8 +1,15 @@
 import React from "react"
+import {useHistory} from "react-router-dom"
 import "../Courses.css"
 
 
 function Main(props) {
+
+  const history = useHistory();
+    function toVideo(e) {
+      e.preventDefault()
+      history.push(props.url)
+    }
 
   return (
     <div className="main mainMain">
@@ -10,6 +17,8 @@ function Main(props) {
       <progress value="33" max="100"></progress>
       </div>
         {props.children}
+        <button className="cancel">Cancel</button>
+        <button onClick={toVideo} className="next">Next</button>
     </div>
   )
   
